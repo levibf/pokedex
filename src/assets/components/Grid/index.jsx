8f807146cbf2cd8a9113d69a1c35b59571b6f0b1
Levi <levibezerra320@gmail.com>
2024-08-02 23:02:26 -0300

    Utilizado MUI para criação do layout

---
+++ src/assets/components/Grid/index.jsx
@@ -0,0 +1,10 @@
+import Grid from '@mui/material/Grid';
+import ImgMediaCard from '../Card';
+
+export default function GridContainer() {
+    return (
+        <Grid container spacing={1}>
+            <ImgMediaCard />
+        </Grid>
+    )
+}
